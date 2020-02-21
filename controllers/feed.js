56fcb3036fa4a9baaa6b2f04c76b4728294f26d3
Jose Cisneros <josecis0216@gmail.com>
2020-02-21 10:12:12 -0700

    changes made to imageUrl

--- controllers/feed.js
+++ controllers/feed.js
@@ -6,16 +6,16 @@ const { validationResult } = require('express-validator/check');
 const Post = require('../models/post');
 
 exports.getPosts = (req, res, next) => {
-  const currentPage = req.query.page || 1;
-  const perPage = 2;
+  // const currentPage = req.query.page || 1;
+  // const perPage = 2;
   let totalItems;
   Post.find()
     .countDocuments()
     .then(count => {
       totalItems = count;
       return Post.find()
-        .skip((currentPage - 1) * perPage)
-        .limit(perPage);
+        // .skip((currentPage - 1) * perPage)
+        // .limit(perPage);
     })
     .then(posts => {
       res
--- models/post.js
+++ models/post.js
@@ -9,7 +9,7 @@ const postSchema = new Schema(
     },
     imageUrl: {
       type: String,
-      required: true
+      required: false
     },
     content: {
       type: String,
